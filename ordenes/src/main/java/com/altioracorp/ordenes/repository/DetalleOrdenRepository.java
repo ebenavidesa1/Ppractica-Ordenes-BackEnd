b01f7cf1c1dd8f499d0604c5186a86a4812eda2e
ebenavidesa1 <76983108+ebenavidesa1@users.noreply.github.com>
2021-04-09 17:56:48 -0500

    Add files via upload

--- ordenes/src/main/java/com/altioracorp/ordenes/repository/DetalleOrdenRepository.java
+++ ordenes/src/main/java/com/altioracorp/ordenes/repository/DetalleOrdenRepository.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.data.repository.Repository;
 
+import com.altioracorp.ordenes.model.Articulo;
+import com.altioracorp.ordenes.model.Cliente;
 import com.altioracorp.ordenes.model.Detalleorden;
 import com.altioracorp.ordenes.model.Orden;
 
@@ -13,6 +15,12 @@ public interface DetalleOrdenRepository extends Repository<Detalleorden, Integer
 	List<Detalleorden> findByOrden(Orden idOrden);
 
 	Detalleorden save(Detalleorden detalle);
+	
+	Detalleorden findByArticulo(Articulo art);
+
+	Detalleorden findById(int id);
+	
+	//Detalleorden findByCantidad(int codigo);
 	
 
 }
