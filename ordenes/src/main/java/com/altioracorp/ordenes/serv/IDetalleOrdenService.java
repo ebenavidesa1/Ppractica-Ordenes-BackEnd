b2678561fc8205bbf37375ff7f43f2596bde030c
ebenavidesa1 <76983108+ebenavidesa1@users.noreply.github.com>
2021-04-09 17:57:39 -0500

    Add files via upload

--- ordenes/src/main/java/com/altioracorp/ordenes/serv/IDetalleOrdenService.java
+++ ordenes/src/main/java/com/altioracorp/ordenes/serv/IDetalleOrdenService.java
@@ -9,5 +9,10 @@ public interface IDetalleOrdenService {
 	Detalleorden add(Detalleorden detalle);
 
 	List<Detalleorden> listarPorOrden(int idorden);
+	
+	Detalleorden listarPorArt(int idArticulo);
 
+	Detalleorden mostrarCantidad(int codigo);
+	
+	
 }
